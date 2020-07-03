c9a6c30d06ae495347a9b33f83d2dd16fe40bf3c
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-03 12:39:07 +0200

    terminando la app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
+import Cancion from './components/Cancion';
+import Info from './components/Info';
 
 function App() {
+  const [busquedaLetra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
+
+  useEffect(() => {
+    if (Object.keys(busquedaLetra).length === 0) return;
+    const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaLetra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      const [letra, info] = await Promise.all([axios(url), axios(url2)]);
+
+      guardarLetra(letra.data.lyrics);
+      console.log(info);
+
+      guardarInfo(info.data.artists[0]);
+      //guardarLetra(resultado.data.lyrics);
+    };
+
+    consultarApiLetra();
+  }, [busquedaLetra, info]);
   return (
     <Fragment>
-      <Formulario />
+      <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,82 @@
-import React from 'react';
-const Formulario = () => {
-  return <h1>Desde formulario</h1>;
+import React, { useState } from 'react';
+const Formulario = ({ guardarBusquedaLetra }) => {
+  const [busqueda, guardarBusqueda] = useState({
+    artista: '',
+    cancion: '',
+  });
+  const [error, guardarError] = useState(false);
+  const actualizarState = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { artista, cancion } = busqueda;
+
+  const buscarInformacion = (e) => {
+    e.preventDefault();
+    if (artista.trim() === '' || cancion.trim() === '') {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    guardarBusquedaLetra(busqueda);
+  };
+  return (
+    <div className="bg-info">
+      {' '}
+      {error ? (
+        <p className="alert alert-danger text-center p-2">
+          Todos los campos son obligatorios
+        </p>
+      ) : null}
+      <div className="container">
+        <div className="row">
+          <form
+            onSubmit={buscarInformacion}
+            className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+          >
+            <fieldset>
+              <legend className="text-center">Buscador letras canciones</legend>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Artista</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="artista"
+                      placeholder="Nombre artista"
+                      onChange={actualizarState}
+                      value={artista}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Canción</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="cancion"
+                      placeholder="Nombre canción"
+                      onChange={actualizarState}
+                      value={cancion}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary float-right">
+                Buscar
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Formulario;
